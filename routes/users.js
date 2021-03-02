2f197bd316591f58814df011b016ab7554d206be
Kukuh Alfian <kukuh@majoo.id>
2021-03-02 22:06:42 +0700

    [ADD] Get user by id

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,7 @@ var router = express.Router();
 const userHandler = require('./handler/user')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/:id', userHandler.getUser);
 
 router.post('/register', userHandler.register)
 router.post('/login', userHandler.login)
